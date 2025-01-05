7f99b9cad67710c3fb86e0943e6b96a80d397629
hoareausylvie <hoareauylvie@outlook.fr>
2025-01-05 14:06:34 +0400

    styles: mise à jour des styles Mobile First

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,31 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import styles from '@/components/Navbar.module.css';
+import '../styles/globals.css';
 
 const Navbar: React.FC = () => {
-    
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    // Fonction pour ouvrir/fermer le menu
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
     return (
         <nav className={styles.nav}>
-            <ul className={styles.navList}>
+            <div>
+                <h1>Choco Péi</h1>
+            </div>
+
+            {/* Icône du hamburger, visible sur mobile */}
+            <div className={`${styles.hamburger} ${isMenuOpen ? styles.open : ''}`} onClick={toggleMenu} aria-label="Ouvrir le menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+
+            {/* Liste de navigation, cachée ou non en fonction du menu */}
+            <ul className={`${styles.navList} ${isMenuOpen ? styles.show : ''}`}>
                 <li className={styles.navItem}><Link href="/" className={styles.navLink}>Accueil</Link></li>
                 <li className={styles.navItem}><Link href="/about" className={styles.navLink}>A propos</Link></li>
                 <li className={styles.navItem}><Link href="/products" className={styles.navLink}>Produits</Link></li>
