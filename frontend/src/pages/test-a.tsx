ba0bb5ea057e098f4c5c7adb30f89abfea9585e0
hoareausylvie <hoareauylvie@outlook.fr>
2025-04-23 21:21:01 +0400

    fix: tentatives de débuggage de l'application

--- frontend/src/pages/test-a.tsx
+++ frontend/src/pages/test-a.tsx
@@ -1,11 +1,15 @@
 // pages/test-a.tsx
 import Link from 'next/link'; // Utilisez Link cette fois pour tester sa fonctionnalité
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 const TestA = () => {
   return (
     <div>
-      <h1>Page Test A</h1>
-      <Link href="/test-b">Aller à la Page Test B</Link>
+        <Navbar />
+        <h1>Page Test A</h1>
+        <Link href="/test-b">Aller à la Page Test B</Link>
+        <Footer />
     </div>
   );
 };
